42a2a861005e0624a7c012346d90765e2a1857bb
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-09-29 12:19:36 +0300

    added some functionals

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    description: 'field for set user name',
+    default: 'Roman',
+    required: false,
+  })
+  name: string;
+
+  @ApiProperty({
+    description: 'field for set user email',
+    default: 'roman@gmail.com',
+  })
+  email: string;
+
+  @ApiProperty()
+  password: string;
+}
