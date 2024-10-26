d6b121dfad77c5f9209bda9253bfc569409c4e2d
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-10-26 16:06:31 +0300

    added id type

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ArticleID, UserID } from 'src/common/types/entity-ids.type';
 
 import { Config } from '../../../configs/config.type';
+import { ArticleRepository } from '../../repository/services/article.repository';
 import { UserRepository } from '../../repository/services/user.repository';
 import { UpdateUserReqDto } from '../models/dto/req/update-user.req.dto';
 
@@ -10,21 +12,25 @@ export class UsersService {
   constructor(
     private readonly configService: ConfigService<Config>,
     private userRepository: UserRepository,
+    private articleRepository: ArticleRepository,
   ) {}
 
-  findOne(id: number) {
+  findOne(id: UserID) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserReqDto) {
-    return `This action updates a #${id} user`;
+  update(id: UserID, updateUserDto: UpdateUserReqDto) {
+    return `This action updates a #${id} ${updateUserDto} user`;
   }
 
-  remove(id: number) {
+  remove(id: UserID) {
     return `This action removes a #${id} user`;
   }
 
-  public async checkAbilityToEditArticle(userId: string, articleId: string) {
-    // Check if the user has permission to edit the article
+  public async checkAbilityToEditArticle(userId: UserID, articleId: ArticleID) {
+    const article = await this.articleRepository.findOne({
+      where: { id: articleId },
+    });
+    return `Check if the user ${userId}has permission to edit the article${article}`;
   }
 }
